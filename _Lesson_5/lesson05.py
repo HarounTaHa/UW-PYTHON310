2276e76874f71989dd1edb5f50836aeea0ccd1d3
HarounTaHa <harountaha@outlook.sa>
2023-03-09 01:26:31 +0200

    practice lesson five :)

---
+++ _Lesson_5/lesson05.py
@@ -0,0 +1,24 @@
+# import my_package
+#
+# print(my_package.name1)
+# print(my_package.name2)
+#
+# import my_package.a_module
+#
+# print(my_package.a_module.name3)
+# print(my_package.a_module.name4)
+# my_package.a_module.a_function()
+
+"""
+But there a handful of ways to import modules and packages.
+
+    import modulename
+
+If you want only a few names in a module, and don't want to type the module name each time, you can import only the names you want:
+
+    from modulename import this, that
+
+"""
+import sys
+import math
+
